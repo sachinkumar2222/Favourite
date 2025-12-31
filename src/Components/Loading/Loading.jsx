d98b34d044d25a089d4c7b0b82179d44da935559
Sachin Kumar <sachin.kumar@sltech.local>
2025-12-31 10:28:17 +0530

    Improve loading state with pure CSS animation

--- src/Components/Loading/Loading.jsx
+++ src/Components/Loading/Loading.jsx
@@ -1,15 +1,12 @@
 import React from 'react'
 import "./Loading.css";
 
-function Loading() {    
+function Loading() {
     return (
-        <>
-            <div className="loading">
-                    <div className="loading__container__content">
-                        <iframe src="https://lottie.host/embed/a40e27d8-d38d-468b-84b1-e8f66b77c1fa/GBy7h8vjCW.lottie"></iframe>
-                    </div>
-            </div>
-        </>
+        <div className="loading">
+            <div className="loading__spinner"></div>
+            <p className="loading__text">Loading...</p>
+        </div>
     )
 }
 
